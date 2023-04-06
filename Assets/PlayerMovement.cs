8b63fa4ec52677ecfabbed091c27101477ea2eff
Vojtech <jezekvoj@gmail.com>
2023-04-06 09:10:26 +0100

    commiting more initial setup stuff

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+
+   
+
     void Start()
     {
         
@@ -13,6 +16,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput;
+        float verticalInput;
+        Vector3 movementUpdate;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Debug.Log("Horizontal: " + horizontalInput + ", Vertical: " + verticalInput);
+
+        movementUpdate = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        transform.position += (movementUpdate * Time.deltaTime);
         
+
     }
 }
